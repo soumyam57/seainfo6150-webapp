5bbcc722b431a22b1eb3692a38d5ef718f58c9ac
Soumya Murugendrappa <46505963+soumyam57@users.noreply.github.com>
2020-10-04 15:59:12 -0700

    added quote and blockquote

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -2,26 +2,22 @@ import React from 'react'
 
 const Article = () => {
   return (
-    
-    <div>  
-      <h1 style={{color:'#00001A'}}>The Statue of Liberty's torch heads to new museum </h1> 
-      
-      <small style={{color:'#787878'}}>By Kate Farley  
-        <a style={{color:'#787878'}} href ="kate.farley@nytimes.com">(kate.farley@nytimes.com)</a>, 
-        <time> November 22, 2018 </time>
-      </small>
-      
-      <h3 style={{color:'#03102E'}}>The Statue of Liberty's original torch is getting a new home</h3>
-      
+
+    <div>
+      <h1>The Statue of Liberty's torch heads to new museum</h1> 
+      <i>by Kate Farley ( <a href="mailto:kate.farley@nytimes.com">kate.farley@nytimes.com</a>)</i><br></br>November 22, 2018
+
+      <p>The Statue of Liberty's original torch is getting a new home.</p>
+
       <p>Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was replaced by a replica in 1984. It's been stored in the statue's pedestal ever since.</p>
 
       <p>But visitors will be able to see the original 3,600-pound torch in all its glory at a new Statue of Liberty Museum, which will open on Liberty Island in May 2019. The torch was transported there by truck last week, along with a replica model of Lady Liberty's face.</p>
 
-      <p>"Although it is not one of the most difficult things we have ever moved," says Douglas Phelps, who oversaw the relocation of the torch, "it is certainly the most important."</p>
+      <p><q> Although it is not one of the most difficult things we have ever moved,</q> says Douglas Phelps, who oversaw the relocation of the torch, <q>it is certainly the most important.</q></p>
 
-      <h3 style={{color:'#03102E'}}>The history of the Statue of Liberty</h3>
+      <h1>The history of the Statue of Liberty</h1>
 
-      <p>Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of _Outstanding Universal Value_.</p>
+      <p>Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of <i>Outstanding Universal Value</i>.</p>
 
       <p>Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel -- yes, the same guy who built Paris's Eiffel Tower.</p>
 
@@ -29,23 +25,28 @@ const Article = () => {
 
       <p>Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.</p>
 
-      <p>"The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal: "Give me your tired, your poor / Your huddled masses yearning to breathe free."</p>
-
+      <p><q> The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy,</q> says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal: </p>
+      
+      <blockquote>
+        Give me your tired, your poor<br></br>Your huddled masses yearning to breathe free.
+      </blockquote>
+      
       <p>In 2017, nearly 4.5 million people visited Liberty Island.</p>
 
       <p>When completed, the Statue of Liberty Museum will be 26,000 square feet of history, inspiration and deeper analysis of the statue's symbolism both in the US and around the world.</p>
 
-      <span>The inspiration gallery, which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower Manhattan, including the World Trade Center.</span>
+      <p>The inspiration gallery, which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower Manhattan, including the World Trade Center.</p>
 
-      <h3 style={{color:'#03102E'}}>Some facts about the Statue of Liberty</h3>
-      
-        <li>Total overall height from the base of the pedestal foundation to the tip of the torch is 305 feet, 6 inches</li>
+      <h1>Some facts about the Statue of Liberty</h1>
+      <ul>
+      <li>Total overall height from the base of the pedestal foundation to the tip of the torch is <b>305 feet, 6 inches</b></li>
 
-        <li>Height of the Statue from her heel to the top of her head is 111 feet, 6 inches</li>
+      <li>Height of the Statue from her heel to the top of her head is 111 feet, 6 inches</li>
 
-        <li>The Statue has a 35-foot waistline</li>
+      <li>The Statue has a <b>35-foot</b> waistline</li>
 
-        <li>Total weight of the Statue of Liberty is 225 tons (or 450,000 pounds)</li>
+      <li>otal weight of the Statue of Liberty is <b>225 tons (or 450,000 pounds)</b></li>
+      </ul>
     </div>
   )
 }
